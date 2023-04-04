7367a5b0142dda3f63c45e802dc189f0b9b80bbd
Aboubaker Hussein Al Khatib <AlKhatibAI@students.rhu.edu.lb>
2023-04-05 00:45:35 +0300

    dd

---
+++ Updatename.sql
@@ -0,0 +1,11 @@
+﻿update Product
+set Name = ' T-Shirt' where PID = 001;
+
+update Product
+set Name = 'Pants' where PID = 002;
+
+update Product 
+set Name = 'Gloves' where PID= 003;
+
+update Product 
+set Name = 'Jackets' where PID = 004;
